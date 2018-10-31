1e15bd01e6e3d912907c4342de4296c0ef997f30
Yo Iida <yoiida333@gmail.com>
2018-10-31 01:09:21 +0000

    prepare for travis test

---
+++ test/travis_package_make.bash
@@ -0,0 +1,10 @@
+#!/bin/bash -xve
+
+#sync and make
+rsync -av ./ ~/catkin_ws/src/pimouse_vision_control/
+
+cd ~/catkin_ws/src/
+git clone --depth=1 https://github.com/y011d4/pimouse_ros.git
+
+cd ~/catkin_ws
+catkin_make
